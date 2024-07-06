1eff726b7df3b59335458f9e640550a38a5cbc54
ttangs83 <lgylovely@gmail.com>
2024-07-06 11:39:09 +0900

    0706

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <h2> App.vue문서</h2>
     <img src="./assets/images/main_4.jpg" height="150" width="300">
     <p></p>
-    <input type="text" v-model="todoText" size=15 @keyup.enter="insertTodo">
+    <input type="text" ref="todoText" size=15 @keyup.enter="insertTodo">
+    <!--원본정답 <input type="text" v-model="todoText" size=15 @keyup.enter="insertTodo">-->
     <button @click="insertTodo">todo 등록</button>
     
     
@@ -13,8 +14,11 @@
     </div>
 
     <hr color="blue" size="3"/>
-    <TodoTest v-for ="(item) in todoList" :key="item.id" />
+    <!--App문서에서 구현은 했지만 deleteTodo(id) greenCheckbox({id,checked})-->
+    <TodoTest v-for ="(item) in todoList" :key="item.id"
+              :skytodo = "item" @click-delete="deleteTodo" @toggle-checkbox="greenCheckbox">
 
+    </TodoTest>
     <p></p>
     <Hello msg="모란백합 Welcome"/>
    
@@ -22,14 +26,14 @@
 </template>
 
 <script>
-import Hello from './components/HelloWorld.vue'
+//import Hello from './components/HelloWorld.vue'
 // import BoardTest from './components/BoardTest.vue'
 import TodoTest from './components/TodoTest.vue';
 
 export default {
   name: 'App',
  // components: { Hello , BoardTest}
-  components:{ Hello, TodoTest},
+  components:{TodoTest},
   
   data(){
   return {
@@ -37,8 +41,8 @@ export default {
     todoText : 'abc'  ,
     count : 1 ,
     todoList : [
-        { id:1, text:"kim", checked:'false'},
-        { id:2, text:"lee", checked:'true'},
+        { id:1, text:"kim", checked:false},
+        { id:2, text:"lee", checked:true},
     ]
   }
  },
@@ -54,13 +58,27 @@ export default {
     this.todoList.push({
       id:max+1,
       //id:this.todoList.length+1,
-      text:this.todoText,
+      //text:this.todoText,
+      text : this.$refs.todoText.value,
       checked:false
     }); //배열에 넣기
-    this.todoText = '';
+    //this.todoText = '';
+    this.$refs.todoText.value='';
+    localStorage.setItem("young",JSON.stringify(this.todoList));
     
   },
-  //deleteTodo(id){ //웹브라우저 storage연결
+  deleteTodo(id){ //웹브라우저 storage연결
+     var max = this.todoList.reduce(function (a,b) {
+      
+      return a > b.id ? a : b.id; 
+
+     });
+
+     id=max;
+     var index = this.todoList.findIndex(mytodo=>{return mytodo.id==id;});
+     this.todoList.splice(index,1);
+     localStorage.removeItem(index);
+    
     //var max = this.todoList.reduce(function(a,b){
     //  console.log(a);
     //  return a > b ? a: b.id;
@@ -70,9 +88,11 @@ export default {
     //id = max;
     //var index = this.todoList.findIndex(); // 7월6일 토요일 삭제처리 여기서 부터 시작
     //this.todoList.splice(index,1);
-  //},
-  greenCheckbox(){
-    console.log("greenCheckbox() 함수");
+  },
+  greenCheckbox({id, checked}){
+    //console.log("greenCheckbox() 함수");
+    var index = this.todoList.findIndex(mytodo=>{return mytodo.id==id;});
+    this.todoList[index].checked = checked;
   },
  }
  
--- src/components/TodoTest.vue
+++ src/components/TodoTest.vue
@@ -1,32 +1,41 @@
 <template lang="">
     <div>
-
+        <!--:skytodo = "item" @click-delete="deleteTodo" @toggle-checkbox="greenCheckbox"-->
+        <input type="checkbox" :checked="skytodo.checked" @change="myToggleCheckbox"> {{skytodo.checked}}
+        <span :style="skytodo.checked ? 'text-decoration:line-through' : '' ">
+            {{skytodo.id}} - {{skytodo.text}}
+        </span>
+        <button @click="myDeleteTodo"> 삭제 </button>
 
     </div>
 </template>
 <script>
     export default {
         name: 'TodoTest',
-        data(){
-            return {
-                lotto: 1 ,
-            }
+        props:{
+            skytodo:{ type:Object, required:true} ,
         },
         methods:{
-            deleteTodo(){
-                console.log('TodoTest.vue deleteTodo함수');
-                alert('TodoTest.vue deleteTodo함수');
+           // <!--:skytodo = "item" @click-delete="deleteTodo" @toggle-checkbox="greenCheckbox"-->
+            myDeleteTodo(){
+                console.log('TodoTest.vue myDeleteTodo함수');
+                //alert('TodoTest.vue deleteTodo함수');
                 //이해해야 하는 함수 this.$emit(인자1,인자2)
+                this.$emit('click-delete', this.skytodo.id);
                 
             },
-            toggleCheckbox(){
-                console.log('TodoTest.vue toggleCheckbox함수');
-                alert('TodoTest.vue toggleCheckbox함수');
+            myToggleCheckbox(event){
+                console.log('TodoTest.vue myToggleCheckbox함수');
+                //alert('TodoTest.vue toggleCheckbox함수');
                 //이해해야 하는 함수 this.$emit(인자1,인자2)
+                this.$emit('toggle-checkbox',{
+                    id:this.skytodo.id ,
+                    checked:event.target.checked
+                });
             }
         },
     }
 </script>
-<style lang="scss" scoped>
+<style lang="">
     
 </style>
